4ddb28938ae2bab30fa3dc1d19addfedc8f0bebd
ManuLores <loresrivas14@gmail.com>
2022-03-30 16:39:41 +0200

    added submit city name and country code

--- WeatherApp/src/app/app.component.ts
+++ WeatherApp/src/app/app.component.ts
@@ -7,16 +7,46 @@ import { WeatherAppService } from './services/weather-app.service'
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor(private waService: WeatherAppService) {
 
+export class AppComponent implements OnInit {
+  code: string = "";
+  city: string = "";
+
+  weatherParameters = {
+    Weather: "",
+    Temperature: 0,
+    maxTemperature: 0,
+    minTemperature: 0,
+    thermalSense: 0,
+    Pressure: 0,
+    Humidity: 0,
+    Altitude: 0,
+    Latitude: 0
+  }
+
+  constructor(private waService: WeatherAppService) {
   }
 
   ngOnInit() {
-    this.waService.getWeather('Zaragoza', 'es').subscribe(
-      res => console.log(res),
+    
+  }
+
+  searchCity() {
+    console.log(this.city, this.code)
+    this.waService.getWeather(this.city, this.code).subscribe(
+      result => {
+        let res: any = result;
+        this.weatherParameters.Weather = res.weather[0].main;
+        this.weatherParameters.Temperature = res.main.temp;
+        this.weatherParameters.maxTemperature = res.main.temp_max;
+        this.weatherParameters.minTemperature = res.main.temp_min;
+        this.weatherParameters.thermalSense = res.main.feels_like;
+        this.weatherParameters.Pressure = res.main.pressure;
+        this.weatherParameters.Humidity = res.main.humidity;
+        this.weatherParameters.Altitude = res.coord.lat;
+        this.weatherParameters.Latitude = res.coord.lon;
+      },
       err => console.log(err)
     )
-  
   }
 }
--- WeatherApp/src/app/app.module.ts
+++ WeatherApp/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
